53b6125a1e44db4b6a009fd9c8b5875422474801
Cody Boyko <codyboyko@yahoo.ca>
2017-11-29 21:40:53 +0000

    Step through quiz and mark score.

--- components/CardQuiz.js
+++ components/CardQuiz.js
@@ -5,20 +5,24 @@ import { connect } from 'react-redux'
 class CardQuiz extends Component {
   state = {
     cardIndex: 0,
-    numberCorrect: 0
+    numberCorrect: 0,
+    showAnswer: false
   }
     
   handleAnswerToggle = () => {
     // Toggle between the question and answer.
+    this.setState((state) => ({
+      showAnswer: !state.showAnswer
+    }))
   }
 
   handleCorrect = () => {
     // Move to next question.
     
     // Set quiz state.
-    this.setState(() => ({
-      cardIndex: '',
-      numberCorrect: ''
+    this.setState((state) => ({
+      cardIndex: state.cardIndex + 1,
+      numberCorrect: state.numberCorrect + 1
     }))    
   }
   
@@ -26,31 +30,41 @@ class CardQuiz extends Component {
     // Move to next question.
     
     // Set quiz state.
-    this.setState(() => ({
-      cardIndex: '',
-      numberCorrect: ''
+    this.setState((state) => ({
+      cardIndex: state.cardIndex + 1
     }))    
   }  
     
-  render() {
-    const { cardIndex, numberCorrect } = this.state
+    render() {
+    const { cardIndex, numberCorrect, showAnswer } = this.state
     const { deck } = this.props
+    
     return (
       <View behavior='padding'>
-        <Text>{cardIndex + 1} / {deck.questions.length}</Text>
-        <Text>Question Goes Here</Text>
-        <TouchableOpacity 
-          onPress={this.handleAnswerToggle} >
-          <Text>Answer</Text>
-        </TouchableOpacity>
-        <TouchableOpacity 
-          onPress={this.handleCorrect} >
-          <Text>Correct</Text>
-        </TouchableOpacity>
-        <TouchableOpacity 
-          onPress={this.handleIncorrect} >
-          <Text>Incorrect</Text>
-        </TouchableOpacity>        
+        {deck.questions.length > cardIndex ?
+          <View>
+            <Text>{cardIndex + 1} / {deck.questions.length}</Text>
+            {showAnswer === true ?
+              <Text>{deck.questions[cardIndex].answer}</Text>
+            : 
+              <Text>{deck.questions[cardIndex].question}</Text>
+            }          
+            <TouchableOpacity 
+              onPress={this.handleAnswerToggle} >
+              <Text>Answer</Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+              onPress={this.handleCorrect} >
+              <Text>Correct</Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+              onPress={this.handleIncorrect} >
+              <Text>Incorrect</Text>
+            </TouchableOpacity>   
+          </View>
+        :
+          <Text>{(numberCorrect / deck.questions.length * 100).toFixed(2)} % Correct!</Text>
+        }
       </View>
     )
   }
